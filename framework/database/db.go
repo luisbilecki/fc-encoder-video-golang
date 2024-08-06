e167c4477a4eceec5b9589878552dafa9c05503b
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-08-05 22:56:22 -0300

    feat: configure gorm persistence layer

---
+++ framework/database/db.go
@@ -0,0 +1,56 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	_ "gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	Db            *gorm.DB
+	Dsn           string
+	DsnTest       string
+	DbType        string
+	DbTypeTest    string
+	AutoMigrateDb bool
+	Env           string
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func NewDbTest() *gorm.DB {
+	dbInstance := NewDb()
+	dbInstance.Env = "test"
+	dbInstance.DbTypeTest = "sqlite3"
+	dbInstance.DsnTest = ":memory:"
+	dbInstance.AutoMigrateDb = true
+	connection, err := dbInstance.Connect()
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (d *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if d.Env != "test" {
+		d.Db, err = gorm.Open(postgres.Open(d.Dsn), &gorm.Config{})
+	} else {
+		d.Db, err = gorm.Open(sqlite.Open(d.DsnTest), &gorm.Config{})
+	}
+	if err != nil {
+		return nil, err
+	}
+	if d.AutoMigrateDb {
+	}
+
+	return d.Db, nil
+
+}
