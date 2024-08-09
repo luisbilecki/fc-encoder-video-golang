4872ad6db06b036f6c6699336ab037fe9940f35c
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-08-09 18:34:41 -0300

    feat: initialize job implementation service

---
+++ application/services/job_service.go
@@ -0,0 +1,45 @@
+package services
+
+import (
+	"github.com/luisbilecki/fc-encoder-video-golang/application/repositories"
+	"github.com/luisbilecki/fc-encoder-video-golang/domain"
+)
+
+type JobService struct {
+	Job           *domain.Job
+	JobRepository repositories.JobRepository
+	VideoService  VideoService
+}
+
+func (j *JobService) Start() error {
+	return nil
+}
+
+func (j *JobService) performUpload() error {
+	return nil
+}
+
+func (j *JobService) changeJobStatus(status string) error {
+	var err error
+
+	j.Job.Status = status
+	j.Job, err = j.JobRepository.Update(j.Job)
+
+	if err != nil {
+		return j.failJob(err)
+	}
+
+	return nil
+}
+
+func (j *JobService) failJob(error error) error {
+	j.Job.Status = "FAILED"
+	j.Job.Error = error.Error()
+
+	_, err := j.JobRepository.Update(j.Job)
+	if err != nil {
+		return err
+	}
+
+	return error
+}
